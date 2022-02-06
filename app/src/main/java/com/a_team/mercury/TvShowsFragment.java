44c44e7586b0945416eb4f44222f2a315232acbd
Atharva Hudlikar <atharvahudlikar@gmail.com>
2022-02-06 20:36:50 +0530

    tv show fragment works

--- app/src/main/java/com/a_team/mercury/TvShowsFragment.java
+++ app/src/main/java/com/a_team/mercury/TvShowsFragment.java
@@ -2,63 +2,63 @@ package com.a_team.mercury;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TvShowsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.orhanobut.hawk.Hawk;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TvShowsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+    List<CardData> all_card_data;
 
     public TvShowsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TvShowsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TvShowsFragment newInstance(String param1, String param2) {
-        TvShowsFragment fragment = new TvShowsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tv_shows, container, false);
+        View view = inflater.inflate(R.layout.fragment_tv_shows, container, false);
+        all_card_data = Hawk.get("all_data");
+        Log.d("all_data_main", all_card_data.toString());
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.tvshow_recycler_container);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        List<CardData> tvshow_cards = new ArrayList<CardData>();
+        for(int i = 0; i < all_card_data.size(); i++) {
+            Log.d("type_ids", all_card_data.get(i).getType_id());
+            if(all_card_data.get(i).getType_id().equals("tvshow")) {
+                tvshow_cards.add(all_card_data.get(i));
+            }
+        }
+        CardDataAdapter cardDataAdapter = new CardDataAdapter(tvshow_cards, getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(cardDataAdapter);
+        Log.d("itemCount", String.valueOf(cardDataAdapter.getItemCount()));
     }
 }
